2249f2d4cc4438147f9d71088efc8faa8c7961d6
mkrishnappa <mkrishnappa@gametaco.com>
2021-12-13 18:47:04 +0530

    init repo

---
+++ readme.md
@@ -0,0 +1,3 @@
+Started working on the tutorials from the following site:
+
+https://dr0id.bitbucket.io/legacy/pygame_tutorial00.html 
---
+++ src/main.py
@@ -0,0 +1,46 @@
+# import the pygame module, so you can use it
+import os
+import pygame
+
+# define a main function
+class main():
+
+    _running = False
+
+    def _initialize(self):
+        # initialize the pygame module
+        pygame.init()
+        # load and set the logo
+        # logo = pygame.image.load("logo32x32.png")
+        # pygame.display.set_icon(logo)
+        pygame.display.set_caption("Darkryder's Awesome Game")
+        
+        # create a surface on screen that has the size of 240 x 180
+        self._screen = pygame.display.set_mode((800,600))
+        
+        # define a variable to control the main loop
+        self._running = True
+    
+    def _loadAssets(self):
+        asset_path = os.path.join('./assets', 'Face.png')
+        _face = pygame.image.load(asset_path)
+        pass
+    
+    def __init__(self):
+        self._initialize()
+        self._loadAssets()
+        # main loop
+        while self._running:
+            # event handling, gets all event from the event queue
+            for event in pygame.event.get():
+                # only do something if the event is of type QUIT
+                if event.type == pygame.QUIT:
+                    # change the value to False, to exit the main loop
+                    self._running = False
+     
+     
+# run the main function only if this module is executed as the main script
+# (if you import this as a module then nothing is executed)
+if __name__=="__main__":
+    # call the main function
+    main = main()
