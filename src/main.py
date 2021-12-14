b294266eaf32312fc48de412006fca7d70813f25
mkrishnappa <mkrishnappa@gametaco.com>
2021-12-14 11:20:36 +0530

    core loop - WIP

--- src/main.py
+++ src/main.py
@@ -1,11 +1,16 @@
 # import the pygame module, so you can use it
 import os
 import pygame
+from pygame import color
 
 # define a main function
 class main():
 
     _running = False
+    _screenWidth = 800
+    _screenHeight = 600
+    # Cornflower blue
+    _bgColor = [100, 149, 237]
 
     def _initialize(self):
         # initialize the pygame module
@@ -16,16 +21,24 @@ class main():
         pygame.display.set_icon(logo)
         pygame.display.set_caption("Darkryder's Awesome Game")
         
-        # create a surface on screen that has the size of 240 x 180
-        self._screen = pygame.display.set_mode((800,600))
+        # create a surface on screen that has the size of _screenWidth x _screenHeight
+        self._screen = pygame.display.set_mode((self._screenWidth,self._screenHeight))
         
         # define a variable to control the main loop
         self._running = True
     
     def _loadAssets(self):
         asset_path = os.path.join('./assets', 'Face.png')
-        _face = pygame.image.load(asset_path)
+        self._face = pygame.image.load(asset_path)
+
+    def _update(self):
         pass
+
+    def _draw(self):
+         # fill the screen with cornflower blue first
+        self._screen.fill(self._bgColor)
+        self._screen.blit(self._face, (50,50))
+        pygame.display.flip()
     
     def __init__(self):
         self._initialize()
@@ -38,6 +51,9 @@ class main():
                 if event.type == pygame.QUIT:
                     # change the value to False, to exit the main loop
                     self._running = False
+            self._update()
+            self._draw()
+
      
      
 # run the main function only if this module is executed as the main script
